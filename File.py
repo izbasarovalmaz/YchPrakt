76ce71af8020b79bb288da1e7af1e92eb015345d
Almaz <izbasarovalmaz1@gmail.com>
2026-03-04 12:09:04 +0300

    6 Транзакции и управление данными

--- File.py
+++ File.py
@@ -4,13 +4,38 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-# Выбираем пользователей с неизвестным возрастом
-cursor.execute('SELECT * FROM Users WHERE age IS NULL')
-unknown_age_users = cursor.fetchall()
-
-# Выводим результаты
-for user in unknown_age_users:
-    print(user)
+try:
+    # Начинаем транзакцию
+    cursor.execute('BEGIN')
+    
+    # Выполняем операции
+    cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', 
+                   ('user1', 'user1@example.com'))
+    cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', 
+                   ('user2', 'user2@example.com'))
+    
+    # Подтверждаем изменения
+    cursor.execute('COMMIT')
+except:
+    # Отменяем транзакцию в случае ошибки
+    cursor.execute('ROLLBACK')
 
 # Закрываем соединение
 connection.close()
+
+
+# Устанавливаем соединение с базой данных
+with sqlite3.connect('my_database.db') as connection:
+    cursor = connection.cursor()
+    
+    try:
+        # Начинаем транзакцию автоматически
+        with connection:
+            # Выполняем операции
+            cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', 
+                          ('user3', 'user3@example.com'))
+            cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', 
+                          ('user4', 'user4@example.com'))
+    except:
+        # Ошибки будут приводить к автоматическому откату транзакции
+        pass
