6575779f68d9b740e682beff25ea63d01a622b04
Almaz <izbasarovalmaz1@gmail.com>
2026-03-04 11:43:53 +0300

    4B Комбинирование операторов

--- File.py
+++ File.py
@@ -3,39 +3,14 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-# Выбираем имена и возраст пользователей старше 25 лет
-cursor.execute('SELECT username, age FROM Users WHERE age > ?', (25,))
-results = cursor.fetchall()
-
-for row in results:
-    print(row)
-
-connection.close()
-
-
-connection = sqlite3.connect('my_database.db')
-cursor = connection.cursor()
-
-# Получаем средний возраст пользователей для каждого возраста
-cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age')
-results = cursor.fetchall()
-
-for row in results:
-    print(row)
-
-# Фильтруем группы по среднему возрасту больше 30
-cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?', (30,))
-filtered_results = cursor.fetchall()
-for row in filtered_results:
-    print(row)
-
-connection.close()
-
-connection = sqlite3.connect('my_database.db')
-cursor = connection.cursor()
-
 # Выбираем и сортируем пользователей по возрасту по убыванию
-cursor.execute('SELECT username, age FROM Users ORDER BY age DESC')
+cursor.execute('''
+    SELECT username, age, AVG(age) 
+    FROM Users 
+    GROUP BY age 
+    HAVING AVG(age) > ? 
+    ORDER BY age DESC
+''', (30,))
 results = cursor.fetchall()
 
 for row in results:
