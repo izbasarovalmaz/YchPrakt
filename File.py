e3ff792e1180d112f577c18ee4fdcb0f1208d4e4
Almaz <izbasarovalmaz1@gmail.com>
2026-03-04 11:58:45 +0300

    5А Использование методов Fetchall, fetchone, fetchmany

--- File.py
+++ File.py
@@ -4,13 +4,20 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
+# Выбираем первого пользователя
+cursor.execute('SELECT * FROM Users')
+first_user = cursor.fetchone()
+print(first_user)
+
+# Выбираем первых 5 пользователей
+cursor.execute('SELECT * FROM Users')
+first_five_users = cursor.fetchmany(5)
+print(first_five_users)
+
 # Выбираем всех пользователей
 cursor.execute('SELECT * FROM Users')
-users = cursor.fetchall()
-
-# Выводим результаты
-for user in users:
-    print(user)
+all_users = cursor.fetchall()
+print(all_users)
 
 # Закрываем соединение
 connection.close()
