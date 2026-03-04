cd8585891376390bdcd737d894103c720d549582
Almaz <izbasarovalmaz1@gmail.com>
2026-03-04 11:26:25 +0300

    2А Создание индекса и его роль

--- File.py
+++ File.py
@@ -4,15 +4,8 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-# Создаем таблицу Users
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Users (
-        id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL,
-        email TEXT NOT NULL,
-        age INTEGER
-    )
-''')
+# Создаем индекс для столбца "email"
+cursor.execute('CREATE INDEX idx_email ON Users (email)')
 
 # Сохраняем изменения и закрываем соединение
 connection.commit()
